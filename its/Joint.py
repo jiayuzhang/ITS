5469b1c60d829ed36a8338ac1789fb3477cc61d0
Jay <elst5523183@qq.com>
2012-05-04 16:40:38 -0400

    Added more models

---
+++ its/Joint.py
@@ -0,0 +1,6 @@
+class Joint(Point):
+    def __init__(self,_x,_y):
+        Point.__init__(self,_x,_y)
+
+    def draw(self):
+        pass
--- its/Simulation.py
+++ its/Simulation.py
@@ -44,13 +44,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-from Tkinter import *
 
-def main():
-    root = Tk()
-    sm = Simulation(root)
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
