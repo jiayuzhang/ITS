907709392360670cb88aed638285099abf90a4d3
Jay <elst5523183@qq.com>
2012-05-04 03:26:01 -0400

    code design2

--- its/Controller.py
+++ its/Controller.py
@@ -1,6 +1,8 @@
 class Controller(object):
-    def __init__(self):
+    def __init__(self, config, canvas):
         pass
 
     def tick(self):
+        #do logic on models
+        #call each existing model draw method by passing canvas
         pass
