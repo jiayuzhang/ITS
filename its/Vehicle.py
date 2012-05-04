3dd76f643b93e1d52cbefbfde3c25cea8ac4ac0a
Mengqi Han <hmq_2006@hotmail.com>
2012-05-05 05:10:13 +0700

    first

--- its/Vehicle.py
+++ its/Vehicle.py
@@ -1,12 +1,12 @@
+from Tkinter import *
 class Vehicle(object):
     def __init__(self,_id,_speed):
         self.id = _id
         self.speed = _speed
 
     def draw(self,canvas):
-        pass
+	photo =  PhotoImage(file="car.gif")
+	item = canvas.create_image(50,50,image=photo)
 
     def __repr__(self):
         return "vehicle(%d)"%self.id
-
-
