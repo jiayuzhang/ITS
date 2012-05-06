511dd7ca8af43e4d915ee42e42458ba9d1837b41
Mengqi Han <hmq_2006@hotmail.com>
2012-05-06 18:39:58 -0400

    control

--- its/Route.py
+++ its/Route.py
@@ -1,13 +1,22 @@
 import Tkinter
 class Route(object):
-    def __init__(self,_start,_end,_joints):
+    def __init__(self,_start,_end,_joints,canvas):
         self.start = _start
         self.end = _end
         self.joints = _joints
+        self.canvas = canvas
 
-    def draw(self,canvas):
-        pass
+    def create(self):
+        start = self.start
+        for j in self.joints:
+            end = j
+            drawRoad(start,end)
+            start = end
+            
+        drawRoad(start,self.end)
 
     def __repr__(self):
         pass
 
+    def drawRoad(self, p1, p2):
+        self.canvas.create_line(point1.x*100+50,point1.y*100+50,point2.x*100+50,point2.y*100+50,fill="gray", width=10, joinstyle="round", capstyle="projecting")
--- its/Simulation.py
+++ its/Simulation.py
@@ -7,10 +7,11 @@ import time
 
 class Simulation(Frame):
     def __init__(self, root, config):
-        Frame.__init__(self, root, height=700, width=900)
+        Frame.__init__(self, root)
         self.grid(column=3,row=2, sticky=N+E+S+W)
-        self.status = "stop"
-     
+        self.stopped = True
+        self.started = False
+        
         self.initWidgets()
         self.initEvents()
         self.initController(config)
@@ -21,11 +22,12 @@ class Simulation(Frame):
 
         self.stopBtn = Button(self,text="Stop")
         self.stopBtn.grid(column=2, row=1)
+        self.stopBtn['state'] = "disabled"
 
         self.stepBtn = Button(self,text="Step")
         self.stepBtn.grid(column=3, row=1)
 
-        self.canvas = Canvas(self)
+        self.canvas = Canvas(self, height=700, width=900)
         self.canvas.grid(row=2, column=1, columnspan=3)
 
         self.vehicle = Vehicle(1,"south",self.canvas)
@@ -33,25 +35,47 @@ class Simulation(Frame):
 
     def startPressed(self, event):
         print("start click", threading.currentThread)
-        if self.status == "stop":
-            self.status = "start"
-            self.startBtn["text"] = "Pause"
+        if not self.started:
+            self.toggleBtn(self.stopBtn)
+            self.toggleState()
+            self.started = True
             self.start()
-        elif self.status == "start":
-            self.status = "pause"
-            self.startBtn["text"] = "Resume"
-        elif self.status == "pause":
-            self.status = "start"
-            self.startBtn["text"] = "Pause"
-            
+        else:
+            self.toggleState()
+            if not self.stopped:
+                self.start()
+        
     def stopPressed(self, event):
-        self.status = "stop"
-        pass
+        self.resetState()
+        self.resetBtns()
+        #self.ctrl.reset()
 
     def stepPressed(self, event):
-        self.status = "step"
+        self.start()
         pass
 
+    def resetBtns(self):
+        self.startBtn['text']= "Start"
+        self.stopBtn['state'] = "disabled"
+
+    def resetState(self):
+        self.started = False
+        self.stopped = True
+        
+    def toggleBtn(self, btn):
+        if btn['state'] == "disabled":
+            btn['state'] = "normal"
+        else:
+            btn['state'] = "disabled"
+
+    def toggleState(self):
+        if not self.stopped:
+            self.startBtn["text"] = "Resume"
+            self.stopped = True
+        else:
+            self.startBtn["text"] = "Pause"
+            self.stopped = False
+            
     def initEvents(self):
         self.startBtn.bind("<Button-1>", self.startPressed)
         self.stopBtn.bind("<Button-1>", self.stopPressed)
@@ -66,13 +90,16 @@ class Simulation(Frame):
             #time.sleep a tick time might be couple milliseconds
             self.ctrl.tick()
             self.vehicle.move()
-            time.sleep(0.1)
             self.canvas.update()
+            time.sleep(0.05)
+            if self.stopped:
+                break
 
 def main():
     root = Tk()
     root.title("Intelligent Transportation Simulation")
     sm = Simulation(root, config="init.config")
+
     #sm.start()
 
     root.mainloop()
