015c7ac88459fcf0499a3ba091e65f63d0974304
Jay <elst5523183@qq.com>
2012-05-06 21:08:19 -0400

    route.py merged

--- its/Route.py
+++ its/Route.py
@@ -4,9 +4,4 @@ class Route(object):
         self.start = _start
         self.end = _end
         self.joints = _joints
-<<<<<<< HEAD
-=======
 
-    def __repr__(self):
-        pass
->>>>>>> 1fa1e722c264382e16df8a946a8d277f0b97fe15
